affea1a063728f36a2e4d6557aa45173410de6eb
theimpulson <aayushgupta219@gmail.com>
2019-07-19 13:14:00 +0000

    sdm660-common: Drop aptX blobs

--- sdm660-common/sdm660-common-vendor.mk
+++ sdm660-common/sdm660-common-vendor.mk
@@ -96,8 +96,6 @@ PRODUCT_COPY_FILES += \
     vendor/nokia/sdm660-common/proprietary/lib64/lib-imsvt.so:system/lib64/lib-imsvt.so \
     vendor/nokia/sdm660-common/proprietary/lib64/lib-imsvtextutils.so:system/lib64/lib-imsvtextutils.so \
     vendor/nokia/sdm660-common/proprietary/lib64/lib-imsvtutils.so:system/lib64/lib-imsvtutils.so \
-    vendor/nokia/sdm660-common/proprietary/lib64/libaptXHD_encoder.so:system/lib64/libaptXHD_encoder.so \
-    vendor/nokia/sdm660-common/proprietary/lib64/libaptX_encoder.so:system/lib64/libaptX_encoder.so \
     vendor/nokia/sdm660-common/proprietary/lib64/libdiag_system.so:system/lib64/libdiag_system.so \
     vendor/nokia/sdm660-common/proprietary/lib64/libdpmctmgr.so:system/lib64/libdpmctmgr.so \
     vendor/nokia/sdm660-common/proprietary/lib64/libdpmfdmgr.so:system/lib64/libdpmfdmgr.so \
